e369e83f7a358897dc52b9b3daa71a261828ae0a
Jorge Bejar <jorge@wyeworks.com>
2015-05-21 15:29:55 -0300

    No need to wrap json using the model name as root, if rails app is prepared to handle that

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,8 +23,6 @@ $(function(){
       };
     },
 
-    paramRoot: "todo",
-
     // Toggle the `done` state of this todo item.
     toggle: function() {
       this.save({done: !this.get("done")});
