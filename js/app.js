988f99493d5370dd228037a57f876b0ad4ca5905
Jorge Bejar <jorge@wyeworks.com>
2015-06-05 16:05:23 -0300

    Make sure backbone requests are considered xhr requests in Rails

--- js/app.js
+++ js/app.js
@@ -5,8 +5,19 @@ var ENTER_KEY = 13;
 var ESC_KEY = 27;
 
 $(function () {
-	'use strict';
+  'use strict';
 
-	// kick things off by creating the `App`
-	new app.AppView();
+  // Make sure we always set content type as json
+  Backbone._sync = Backbone.sync;
+
+  Backbone.sync = function(method, model, options) {
+    options.beforeSend = function(xhr){
+      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    };
+    return Backbone._sync(method, model, options);
+  };
+
+
+  // kick things off by creating the `App`
+  new app.AppView();
 });
